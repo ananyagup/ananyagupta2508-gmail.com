ff10307783155bfe5b7f75aff14592678cc51470
ananyagup <63497890+ananyagup@users.noreply.github.com>
2020-04-11 16:44:24 +0530

    Update code.py

--- code.py
+++ code.py
@@ -4,7 +4,7 @@
 from allennlp.predictors.predictor import Predictor
 predictor = Predictor.from_path("https://s3-us-west-2.amazonaws.com/allennlp/models/coref-model-2018.02.05.tar.gz")
 
-sent = "my mother was happy. She gave me a chocolate"
+sent = "my mother was happy. She gave me a pat on the back" #or any other sentence.
 pred = predictor.predict(
     document= sent
 )
@@ -15,7 +15,7 @@ document = pred['document']
 n = 0
 doc = {}
 for obj in document:
-    doc.update({n :  obj})
+    doc.update({n :  obj}) #what I'm doing here is creating a dictionary of each word with its respective index, making it easier later.
     n = n+1
     
 clus_all = []
